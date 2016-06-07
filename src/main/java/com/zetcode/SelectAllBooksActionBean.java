46dfac0f8fb148134e31f097f307b632a5829bf8
Jan Bodnar <jan.bodnar@gmail.com>
2016-06-07 12:06:16 +0200

    Create SelectAllBooksActionBean.java

---
+++ src/main/java/com/zetcode/SelectAllBooksActionBean.java
@@ -0,0 +1,46 @@
+package com.zetcode.action;
+
+import com.zetcode.bean.Book;
+import com.zetcode.services.BookService;
+import java.util.List;
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+
+
+public class SelectAllBooksActionBean implements ActionBean {
+    
+    private static final String VIEW = "/showAllBooks.jsp";
+    private ActionBeanContext context;    
+    private List<Book> books;
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @DefaultHandler
+    public Resolution showAll() {
+        
+        this.books = BookService.getAllBooks();
+
+        return new ForwardResolution(VIEW);
+    }
+
+    @Override
+    public void setContext(ActionBeanContext context) {
+
+        this.context = context;
+    }
+
+    @Override
+    public ActionBeanContext getContext() {
+        
+        return context;
+    }
+}
